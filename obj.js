595700962b168fe7b65ae9f86b707aeabb5c0d19
Aymber McElroy <aymber.mcelroy@gmail.com>
2024-10-04 14:43:06 -0600

    update myStuff

--- obj.js
+++ obj.js
@@ -3,5 +3,5 @@ var myStuff = {
     "Graduate School": {city: "Laramie", state: "Wyoming", "coord": [-105.5690, 41.3147]},
     "Current Residence": {city: "Kalispell", state: "Montana", "coord": [-114.3168, 48.1920]},
     "Favorite Place I've Lived": {city: "Anchorage", state: "Alaska", "coord": [-149.8113, 61.2536]},
-    "Favorite Vacation": {city: "Miami", state: "Florida", "coord": [-125.9066, 49.1530]}
+    "Favorite Vacation": {city: "Tofino", state: "British Columbia", "coord": [-125.9066, 49.1530]}
 }
